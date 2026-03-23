c4ff2022f4da6e68235b745a0e26a395e049ec7c
charlie-robison <charlierobison480@gmail.com>
2026-03-23 13:40:16 -1000

    Fixed run.sh

--- server/run.sh
+++ server/run.sh
@@ -3,12 +3,12 @@ set -e
 
 cd "$(dirname "$0")"
 
-if [ ! -d ".venv" ]; then
+if [ ! -d "venv" ]; then
     echo "Creating virtual environment..."
-    python3 -m venv .venv
+    python3.13 -m venv venv
 fi
 
-source .venv/bin/activate
+source venv/bin/activate
 
 echo "Installing dependencies..."
 pip install -r requirements.txt --quiet
